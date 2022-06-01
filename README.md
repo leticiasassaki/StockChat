a70d28b1a0887cddc281a4cdeafe7f5e623ad796
Leticia Kawamoto <leticiac.0401@gmail.com>
2022-05-31 21:06:15 -0300

    Add readme and update API

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # StockChat
+This is a chat to get and process quotes, using SignalR and RabbitMQ
+
+### Prerequisites
+- .NET 6.0
+```
+https://dotnet.microsoft.com/download/dotnet/6.0
+```
+
+### Running the project
+```bash
+
+# Command to restore the dependencies and tools of a project
+dotnet restore
+
+# Command to run source code without any explicit compile or launch commands.
+dotnet run
+
+# Command to execute unit tests.
+dotnet test
+
+```
+### Docker
+To get things working run the following command `docker-compose up`
+
+# Built With
+
+* [.NET 6.0](https://docs.microsoft.com/en-us/dotnet/core/whats-new/dotnet-6) - The framework used
+* [EntityFrameworkCore](https://docs.microsoft.com/en-us/ef/core/) - ORM
+* [SignalR](https://dotnet.microsoft.com/en-us/apps/aspnet/signalr) - Library for ASP.NET of real-time web functionality to applications
+* [RabbitMQ](https://www.rabbitmq.com/) - Message-broker
+* [Visual Studio 2022](https://visualstudio.microsoft.com/pt-br/vs/) - IDE used to develop the project
---
+++ StockChat.UI/Consumers/StockConsumer.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StockChat.Domain.Dto.External;
+using StockChat.UI.Hubs;
+using System.Text;
+
+namespace StockChat.UI.Consumers
+{
+    public class StockConsumer : BackgroundService
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+        private readonly IConfiguration _configuration;
+
+        public StockConsumer(IHubContext<ChatHub> hubContext,
+                             IConfiguration configuration)
+        {
+            _hubContext = hubContext;
+            _configuration = configuration;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var connectionString = _configuration["Rabbit:StockQueue:ConnectionString"];
+            var queueName = _configuration["Rabbit:StockQueue:QueueName"];
+            var durable = Convert.ToBoolean(_configuration["Rabbit:StockQueue:Durable"]);
+
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(connectionString),
+                DispatchConsumersAsync = true
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: queueName,
+                                durable: durable,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.Received += HandleMessage;
+            channel.BasicConsume(queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+        }
+
+        public async Task HandleMessage(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            var stockData = JsonConvert.DeserializeObject<StockMessageDto>(message);
+
+            if (stockData == null)
+                return;
+
+            if (stockData.StockValue == "N/D")
+                await _hubContext.Clients.Client(stockData.Caller).SendAsync("ReceiveMessage", "stock-bot", "Stock code not found");
+            else
+                await _hubContext.Clients.Client(stockData.Caller).SendAsync("ReceiveMessage", "stock-bot", $"{stockData.StockCode} quote is ${stockData.StockValue} per share");
+        }
+    }
+}
